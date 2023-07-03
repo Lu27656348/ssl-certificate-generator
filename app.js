9ca67497ab29ae72dca6fd393ad074f479dce279
Lu27656348 <luiscarlossomoza@gmail.com>
2023-07-03 09:40:37 -0400

    inicio de proyecto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use("/", (req,res,next) => {
+    res.send("Hello from SSL server")
+});
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    certificate: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
+
+sslServer.listen(3443, () => {
+    console.log("Secure server initializated");
+})
